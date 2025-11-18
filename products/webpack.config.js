172a40be469ca64040ccff789c35e73c4fb17180
Nguyễn Quang Huy <nguyenqunaghuylt2002@gmail.com>
2025-11-18 11:16:42 +0700

    intergrate products into root

--- products/webpack.config.js
+++ products/webpack.config.js
@@ -2,15 +2,23 @@ const { merge } = require("webpack-merge");
 const singleSpaDefaults = require("webpack-config-single-spa-react-ts");
 
 module.exports = (webpackConfigEnv, argv) => {
+  const orgName = "ecom-micro-frontend";
+  const projectName = "products";
+  
   const defaultConfig = singleSpaDefaults({
-    orgName: "ecom-micro-frontend",
-    projectName: "products",
+    orgName,
+    projectName,
     webpackConfigEnv,
     argv,
     outputSystemJS: false,
   });
 
+  defaultConfig.externals = ['single-spa', new RegExp(`^@${orgName}/`)];
+
+  console.log("Default: ", defaultConfig)
+
   return merge(defaultConfig, {
+    externals: [],
     devServer: {
       port: 3001,
       historyApiFallback: true,
